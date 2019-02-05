f614ebb14480cce5c5072f2f751f340105f49915
mapachurro <oliver.renwick@gmail.com>
2019-02-05 12:16:02 -0500

    At this point, POST function in survey.html successfully posts user data to the server; now the server needs to pass that information into some functions, and once we get a result from them, we should be able to pass a res from the server back to the DOM

--- app/public/assets/javascript/eval.js
+++ app/public/assets/javascript/eval.js
@@ -1,54 +1,14 @@
 var path = require("path");
 var friends = require("../../../data/friends");
-var userName = $("#name").val();
-var userPhoto = $("#photo").val();
-var scores = [
-    $("#q1").val(),
-    $("#q2").val(),
-    $("#q3").val(),
-    $("#q4").val(),
-    $("#q5").val(),
-    $("#q6").val(),
-    $("#q7").val(),
-    $("#q8").val(),
-    $("#q9").val(),
-    $("#q10").val()
-];
-
-$("#submit").on("click", function (event) {
-    event.preventDefault();
-    console.log(friends);
-})
 
 
-// function compare(arr1, arr2){
+function compare(arr1, arr2){
 
-//     friends.forEach((friends[e1].scores)=>arr2.forEach((e2)=>
-//     if (e1 === e2){
+    friends.forEach((friends[e1].scores)=>arr2.forEach((e2)=>
+    if (e1 === e2){
         
-//     }))
-// }
-
-// Chosen CSS
-// var config = {
-//     ".chosen-select": {},
-//     ".chosen-select-deselect": {
-//         allow_single_deselect: true
-//     },
-//     ".chosen-select-no-single": {
-//         disable_search_threshold: 10
-//     },
-//     ".chosen-select-no-results": {
-//         no_results_text: "Oops, nothing found!"
-//     },
-//     ".chosen-select-width": {
-//         width: "95%"
-//     }
-// };
-
-// for (var selector in config) {
-//     $(selector).chosen(config[selector]);
-// }
+    }))
+}
 
 // Capture the form inputs
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,6 @@
 var path = require("path");
+var eval = require("../public/assets/javascript/eval")
+
 
 module.exports = function(app) {
   app.get("/survey", function(req, res) {
@@ -8,4 +10,23 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
+
+  // Process the results
+app.post("/results", function(req, res) {
+  console.log(req.body.name)
+  console.log(req.body.pic)
+  console.log(req.body.answers)
+  name = req.body.name;
+  pic = req.body.pic;
+  answers = req.body.answers;
+  
+  
+  // eval.compare(req.)
+
+
+    // Send back the results
+    // res.json({ result });
+    // console.log({ id: result.insertId });
+  });
+
 };
